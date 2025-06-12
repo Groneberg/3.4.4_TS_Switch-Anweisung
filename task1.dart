badc9ad0882973ddf183944d9b2f41666ba7b669
Stefan Groneberg <stefangroneberg@Mac.fritz.box>
2025-06-12 15:09:03 +0200

    removed dead code

--- task1.dart
+++ task1.dart
@@ -7,19 +7,13 @@ void main() {
     case Weekday.monday:
     case Weekday.tuesday:
       print('Montage und Dienstage sind anstrengend');
-      break;
     case Weekday.wednesday:
     case Weekday.thursday:
       print('Mittwoch und Donnerstag sind ok');
-      break;
     case Weekday.friday:
       print('Freitag ist super');
-      break;
     case Weekday.saturday:
     case Weekday.sunday:
       print('Wochenende ist genial');
-      break;
-    default:
-      print('Kein Tag ausgewählt');
-  }
+    }
 }
--- task2.dart
+++ task2.dart
@@ -7,16 +7,12 @@ void main() {
       switch (age) {
         case >= 0 && <= 20:
           print("Im Schnitt 181,4m");
-          break;
         case >= 21 && <= 25:
           print("Im Schnitt 181,3m");
-          break;
         case >= 26 && <= 30:
           print("Im Schnitt 180,4m");
-          break;
         case >= 31 && <= 35:
           print("Im Schnitt 179.8m");
-          break;
         default:
           print("Altersbereich für männlich nicht definiert");
       }
@@ -25,20 +21,15 @@ void main() {
       switch (age) {
         case >= 0 && <= 20:
           print("Im Schnitt 167,5m");
-          break;
         case >= 21 && <= 25:
           print("Im Schnitt 167,3m");
-          break;
         case >= 26 && <= 30:
           print("Im Schnitt 167,2m");
-          break;
         case >= 31 && <= 35:
           print("Im Schnitt 166.8m");
-          break;
         default:
           print("Altersbereich für weiblich nicht definiert");
       }
-      break;
     default:
       print("Geschlecht nicht definiert");
   }
