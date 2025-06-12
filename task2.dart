048feb7999e6cb20b60055c481c4d4cb66770652
Stefan Groneberg <stefangroneberg@Mac.fritz.box>
2025-06-12 11:00:16 +0200

    first commit

---
+++ task2.dart
@@ -0,0 +1,45 @@
+ void main() {
+  String sex = "male"; 
+  int age = 22;
+
+  switch (sex) {
+    case "male":
+      switch (age) {
+        case >= 0 && <= 20:
+          print("Im Schnitt 181,4m"); 
+          break;
+        case >= 21 && <= 25:
+          print("Im Schnitt 181,3m");
+          break;
+        case >= 26 && <= 30:
+          print("Im Schnitt 180,4m");
+          break;
+        case >= 31 && <= 35:
+          print("Im Schnitt 179.8m");
+          break;
+        default:
+          print("Altersbereich für männlich nicht definiert");
+      }
+      break;
+    case "female":
+      switch (age) {
+        case >= 0 && <= 20:
+          print("Im Schnitt 167,5m");
+          break;
+        case >= 21 && <= 25:
+          print("Im Schnitt 167,3m");
+          break;
+        case >= 26 && <= 30: 
+          print("Im Schnitt 167,2m");
+          break;
+        case >= 31 && <= 35:
+          print("Im Schnitt 166.8m");
+          break;
+        default:
+          print("Altersbereich für weiblich nicht definiert");
+      }
+      break;
+    default:
+      print("Geschlecht nicht definiert");
+  }
+}
